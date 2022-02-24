a8fef41b650fcc75a0cd14c820da3fdeb4c705a1
Matteo PARIGI <maparigi@e2r4p22.clusters.42paris.fr>
2022-02-24 16:18:52 +0100

    bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparigi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 19:36:25 by maparigi          #+#    #+#             */
+/*   Updated: 2022/02/24 15:08:29 by maparigi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 512
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strchr(const char *str, int c);
+
+char	*ft_strjoin(char *s1, char *s2, int i, int j);
+char	*ft_get_next(char *str);
+char	*get_next_line(int fd);
+char	*ft_fill_line(char *s);
+
+size_t	ft_strlen(const char *s);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maparigi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 22:02:07 by maparigi          #+#    #+#             */
+/*   Updated: 2022/02/10 21:24:32 by maparigi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return (0);
+	while (str[++i])
+		if (str[i] == c)
+			return (i);
+	return (-1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_fill_line(char *s)
+{
+	char	*line;
+	int		i;
+
+	if (!s || !s[0])
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * i + 2);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	if (s[i] == '\n')
+	{
+		line[i] = s[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+/*remember to add : free(s1);*/
+
+char	*ft_strjoin(char *s1, char *s2, int i, int j)
+{
+	char	*tmp;
+
+	if (!s1)
+	{
+		s1 = (char *)(malloc(sizeof(char) * 1));
+		s1[0] = '\0';
+	}
+	if (!s2 || !s1)
+		return (NULL);
+	tmp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	if (!tmp)
+		return (NULL);
+	while (s1[++i])
+		tmp[i] = s1[i];
+	while (s2[++j])
+		tmp[i + j] = s2[j];
+	tmp[i + j] = '\0';
+	free(s1);
+	return (tmp);
+}
